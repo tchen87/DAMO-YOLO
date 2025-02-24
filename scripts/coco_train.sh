5207e3d0ab3aee60266be01c58bbb381af4927df
tchen87 <tonychen@echopixeltech.com>
2025-02-24 12:58:34 -0800

    Add other models to script

--- scripts/coco_train.sh
+++ scripts/coco_train.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL18_Nm.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL18_Ns.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL20_N.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL20_Nl.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL20_T.py
 python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL25_S.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL35_M.py
+python -m torch.distributed.launch --nproc_per_node=8  tools/train.py -f configs/damoyolo_tinynasL45_L.py
