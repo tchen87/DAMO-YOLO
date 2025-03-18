eadcfbcc2e8e5cc1efc12045902d5ea0b989bb2f
tchen87 <tonychen@echopixeltech.com>
2025-03-18 09:55:39 -0700

    Add a script for converting models from pth to onnx

---
+++ scripts/convertModels.sh
@@ -0,0 +1,7 @@
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL18_Ns.py -c outputModels/damoyolo_tinynasL18_Ns_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL18_Nm.py -c outputModels/damoyolo_tinynasL18_Nm_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL20_Nl.py -c outputModels/damoyolo_tinynasL20_Nl_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL20_T.py -c outputModels/damoyolo_tinynasL20_T_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL25_S.py -c outputModels/damoyolo_tinynasL25_S_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL35_M.py -c outputModels/damoyolo_tinynasL35_M_03182025.pth --batch_size 1
+tools/converter.py --mode onnx -f configs/damoyolo_tinynasL45_L.py -c outputModels/damoyolo_tinynasL45_L_03182025.pth --batch_size 1
