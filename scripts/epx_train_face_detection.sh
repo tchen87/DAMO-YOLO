532de04a8a0672aa41256658de6a0e4d473eeeec
EPX-DEMO-002\Admin <tonychen@echopixeltech.com>
2025-05-29 13:19:18 -0700

    Remove this

--- scripts/epx_train_face_detection.sh
+++ scripts/epx_train_face_detection.sh
@@ -7,9 +7,6 @@ IMG_DIR="datasets/exampleName/images/default"
 TRAIN_NAME="coco_train_example"
 VAL_NAME="coco_val_example"
 export PYTHONPATH=$PWD:$PYTHONPATH
-source activate base #
-conda activate ~/anaconda3/envs/DAMO-YOLO
-#echo python tools/cocosplit.py --having-annotations -s 0.8 ${INSTANCES_PATH} ${TRAIN_JSON_PATH} ${VAL_JSON_PATH}
 python tools/cocosplit.py --having-annotations -s 0.8 ${INSTANCES_PATH} ${TRAIN_JSON_PATH} ${VAL_JSON_PATH}
 python tools/AddDatasetToConfigFiles.py -ti ${IMG_DIR} -ta ${TRAIN_JSON_PATH} -tn ${TRAIN_NAME} -vi ${IMG_DIR} -va ${VAL_JSON_PATH} -vn ${VAL_NAME}
 python -m torch.distributed.launch --nproc_per_node=1  tools/train.py -f configs/damoyolo_tinynasL20_T.py --train_ann ${TRAIN_NAME} --val_ann ${VAL_NAME}
